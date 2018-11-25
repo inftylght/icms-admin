61bdd78e699cc6ebbb0997f5c9ffe7ca88fdb369
jiratip.w <jirateep.w@csithailand.com>
2018-11-25 14:26:53 +0000

    Add angular material (#1)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatToolbarModule,
+    BrowserAnimationsModule,
     BrowserModule
   ],
   providers: [],
