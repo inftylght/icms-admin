f134ecf8c19c5e9a79c6ab80dfff8c05389de4d6
Jiratip.W <jiratip.w@gmail.com>
2019-03-02 12:45:54 +0700

    add yarn instead of npm

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ BASEDIR=$(dirname "$0")
 cd "$BASEDIR"
 git reset --hard || exit "$?"
 git pull || exit "$?"
-npm install
+yarn install
 ng build --prod
 rm -rf /var/www/admin/html/*
 cp -a -f dist/icms-admin/. /var/www/admin/html/
